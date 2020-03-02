5198e533eeb9e21e30625c9db7adb6eeb3dedad2
jroden <joden1995@gmail.com>
2020-03-01 20:08:57 -0500

    revert solutions to intersection & loopDetection

--- src/linkedlist/LoopDetection.java
+++ src/linkedlist/LoopDetection.java
@@ -2,9 +2,5 @@ package linkedlist;
 
 public class LoopDetection {
     //  Problem: Given a circular linked list, implement an algorithm that returns the node at the beginning of the loop
-    //  Solution: Iterate through the linked list, and extract the nodes into a Node array.  If any of the nodes already exist in
-    //  the array, we know this is the start of the circle and return that node
-    //  Time Complexity: O(n) since we are only looping through the list
-    //  Space Complexity: O(n) since we are storing the input linkedList
 
 }
