d83ad1abeb0a5c3f3374949e1c81cddf822f2492
jroden <joden1995@gmail.com>
2020-03-08 16:10:29 -0400

    Tree data structure class & minimalTree

---
+++ src/Trees/Node.java
@@ -0,0 +1,36 @@
+package Trees;
+
+public class Node {
+    public int value;
+    public Node left;
+    public Node right;
+
+    public Node(int value){
+        this.value = value;
+    }
+
+    public static void printTreeInOrderDFS(Node n){
+        if(n != null){
+            printTreeInOrderDFS(n.left);
+            System.out.println(n.value);
+            printTreeInOrderDFS(n.right);
+        }
+    }
+
+    public static void printTreePreOrderDFS(Node n){
+        if(n != null){
+            System.out.println(n.value);
+            printTreeInOrderDFS(n.left);
+            printTreeInOrderDFS(n.right);
+        }
+    }
+
+    public static void printTreePostOrderDFS(Node n){
+        if(n != null){
+            printTreeInOrderDFS(n.left);
+            printTreeInOrderDFS(n.right);
+            System.out.println(n.value);
+        }
+    }
+
+}
---
+++ src/Trees/Tree.java
@@ -0,0 +1,6 @@
+package Trees;
+
+public class Tree {
+    public Node head;
+
+}
