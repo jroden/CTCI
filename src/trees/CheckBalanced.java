478d18322262f0229115968e1f4fab1ad043ba62
jroden <joden1995@gmail.com>
2020-03-15 12:11:09 -0400

    CheckBalanced defect fix

--- src/trees/CheckBalanced.java
+++ src/trees/CheckBalanced.java
@@ -11,8 +11,7 @@ import static trees.Node.breadthFirstSearch;
 public class CheckBalanced {
     // Problem: Implement a function to check if a binary tree is balanced.  For this question a balanced tree is a tree
     //  where the heights of the twi subtrees of any node never differ by more than one
-    // Solution: For a perfect binary tree know that each layer of the tree should have 2*n the number of elements in the previous layer
-    //  thus we can determine, if we encounter a layer with less than 2*n elements, there may only be one additional layer- else(fail)
+    // Solution: calculate the max height for each node in the tree using DFS, if any differs by more than 1 we know we don't have a complete tree
     // Time Complexity: O(n) because we are doing a breadth-first search
     // Space Complexity: O(n) because we are only storing the input tree
 
@@ -21,15 +20,49 @@ public class CheckBalanced {
         int[] inputArray = {1,2,3,4,5,6,7,8,9};
         Node n = createMinimalTree(inputArray, 0, inputArray.length - 1);
         // test with balanced tree
-        System.out.println(isTreeBalanced(n));
+//        System.out.println(isTreeBalanced(n));
         // test with rudimentary unbalanced tree
-        n = new Node(3);
-        n.left = new Node(3);
+//                    0
+//                  /   \
+//                  1     2
+//                 / \     \
+//                3   4     3
+//               /           \
+//              5             4
+//             /
+//            6
+        n = new Node(0);
+        n.left = new Node(1);
         n.left.left = new Node(3);
+        n.left.left.left = new Node(5);
+        n.left.left.left.left = new Node(6);
+        n.left.right = new Node(4);
+        n.right = new Node(2);
+        n.right.right = new Node(3);
+        n.right.right.right = new Node(4);
         System.out.println(isTreeBalanced(n));
     }
 
-    public static boolean isTreeBalanced(Node n){
+    public static boolean isTreeBalanced(Node n) {
+        // recurse through the tree, if lengths of left & right node differ by more than 1 return false
+        if(n != null) {
+//        System.out.println("left height: " + (getHeight(n.left) + 1));
+//        System.out.println("right height: " + (getHeight(n.right) + 1));
+        if(Math.abs(getHeight(n.left) - getHeight(n.right)) > 1){ System.out.println("returning false"); return false; }
+        }
+        System.out.println("here");
+        if(n.left != null){ return isTreeBalanced(n.left); }
+        if(n.right != null){ return isTreeBalanced(n.right); }
+        return true;
+    }
+
+    private static int getHeight(Node n){
+        if(n == null){ return -1; }
+        return Math.max(getHeight(n.left), getHeight(n.right)) + 1;
+    }
+
+    // if each layer has 2* the number of nodes of the previous layer with 1 exception allowed
+    public static boolean areLayersBalanced(Node n){
         boolean imbalanceEncountered = false;
         Queue<Node> currentQueue = new java.util.LinkedList<>();
         currentQueue.add(n);
--- src/trees/ListOfDepths.java
+++ src/trees/ListOfDepths.java
@@ -61,12 +61,4 @@ public class ListOfDepths {
         return linkedListArray;
     }
 
-
-    static boolean isPowerOfTwo(int n)
-    {
-        if(n==0)
-            return false;
-        return (int)(Math.ceil((Math.log(n) / Math.log(2)))) ==
-                (int)(Math.floor(((Math.log(n) / Math.log(2)))));
-    }
 }
